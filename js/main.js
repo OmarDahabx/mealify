10642d7ebb1131c1328ce0ce00b8fa93bcf3ab0b
omarEssam <omar.dahabxx@gmail.com>
2025-02-04 07:11:04 +0200

    my first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".navbar-nav a");
+
+  links.forEach((link) => {
+    link.addEventListener("click", function () {
+      links.forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      this.classList.add("active");
+    });
+  });
+});
